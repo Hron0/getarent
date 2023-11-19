233a293f91d95266a7c3c4cc81e1644b0ccca62c
Hron0 <sasha1254370@gmail.com>
2023-11-19 21:34:43 +0300

    L

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <nav className="flex flex-row justify-between px-[2%] pt-[15px]">
+      <img src={logo} alt="Logo" width={100} height={100}/>
+
+      
+    </nav>
+  )
+}
+
+export default Navbar
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }/* ,
+  {
+    path: "/blog",
+    element: <Deck />
+  },
+  {
+    path: "/park"
+    element: < />
+  },
+  {
+    path: "/profile/:id"
+    element: < />
+  },
+  {
+    path: "/parking/"
+    element: < />
+  } */
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
