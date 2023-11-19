8ef2c263d9b71b53827da5ee1d3bf8521f41d658
Hron0 <sasha1254370@gmail.com>
2023-11-19 19:02:49 +0300

    ,.

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -1,17 +1,26 @@
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import logoO from "../../public/logoO.svg"
 
-const routes = [
-  {title: 'HomePage', href: '#site'},
-  {title: 'Hosting', href: '#host'},
-  {title: 'Parking', href: '#park'}
+const routes = [  /* TODO Перенести в отдельный файл */
+  { query: 'HomePage', pathname: '/' },
+  { query: 'Hosting', pathname: '/host' },
+  { query: 'Parking', pathname: '/park' }
 ]
 
 const Navbar = () => {
   return (
-    <header>
-      <Link href={'/'}>HomePage</Link>
-    </header>
+    <nav className='flex flex-row justify-between w-full px-[5%] pt-[15px]'>
+      <Image src={logoO} width={100} height={100} alt="logo"/>
+      <div className="flex flex-row gap-[20px]">
+        <div className="flex flex-row gap-[20px]">
+          <Link href="/host">Сдать авто в аренду</Link>
+          <Link href="/">О компании</Link>
+          <Link href="/park">Блог</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
