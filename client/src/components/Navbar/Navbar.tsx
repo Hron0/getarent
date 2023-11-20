b32b15d3e15d365a512b0d2f6113b0ee65b4250f
Hron0 <sasha1254370@gmail.com>
2023-11-20 19:29:21 +0300

    xdd

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 import logo from "../../assets/logo.svg"
+import useWindowDimensions from "@/hooks/useWindowDimensions"
 
 const Navbar = () => {
+/*   const { width } = useWindowDimensions() */
+
   return (
     <nav className="flex flex-row justify-between px-[2%] pt-[15px]">
-      <img src={logo} alt="Logo" width={100} height={100}/>
+      <img src={logo} alt="Logo" width={50} height={50} />
+      <div className="flex flex-row items-center">
+        <div className="flex flex-row gap-4 items-center">
+          <Link to="/park">Сдать авто в аренду</Link>
+          <Link to="/">О компании</Link>
+          <Link to="/blog">Блог</Link>
+        </div>
+      </div>
 
-      
     </nav>
   )
 }
